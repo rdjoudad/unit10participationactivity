3adda49adcf54097b1fa1901205f27bfc9a83a69
Ryma Djoudad <zenyod85@gmail.com>
2026-03-29 16:58:16 -0400

    Added new_username

--- remember_me.py
+++ remember_me.py
@@ -10,21 +10,25 @@ def get_stored_username(path):
         return None
 
 def get_new_username(path):
-    username = input("What is your name? ")
-    contents = json.dumps(username)
+    new_username = input("What is your name? ")
+    contents = json.dumps(new_username)
     path.write_text(contents)
-    return username
+    new_user_path = Path("guest_book.txt")
+    with new_user_path.open("a") as file:
+        file.write(new_username + "\n")
+    return new_username
+    
 
 def greet_user():
     path = Path("username.json")
     username = get_stored_username(path)
     if username:
         user_response = input(f"Are you {username}? (y/n)")
+        new_username = get_new_username(path)
         if user_response == "y":
-            print(f"Hello, {username}! it's nice to see you again")
+            print(f"Hello, {username}! it's nice to see you again.")
         else:
-            print("What is your name? ")
-        print(f"Welcome, {username}! We'll be sure to remember you next time.")
+            print(f"Welcome, {username}! We'll be sure to remember you next time.")
     else:
         username = get_new_username(path)
         print(f"We'll remember you when you come back, {username}!")
